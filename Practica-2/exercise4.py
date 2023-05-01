3e27485f06bc7fedd0a21bb8aea1413ea667f9fc
luciano-meloclaps <meloclapsluciano@gmail.com>
2023-05-01 13:12:32 -0300

    subo los ejercicios de la practica 4

---
+++ Practica-2/exercise4.py
@@ -0,0 +1,109 @@
+"""Expresiones Booleanas."""
+
+
+def es_vocal_if(letra: str) -> bool:
+    """Toma un string y devuelve un booleano en base a si letra es una vocal o
+    no.
+
+    Restricciónes:
+        - Utilizar un if para cada posibilidad.
+        - Utilizar la función lower() sólo una vez.
+        - No utilizar ELSE.
+        - Utilizar 6 returns.
+
+    Referencia: https://docs.python.org/3/library/stdtypes.html#string-methods
+    """
+    
+   
+    if letra == "a":
+        return True
+
+    if letra == "b":
+        return False
+    
+    if letra == "A":
+        letra = letra.lower()
+        return True
+    
+    if letra == "e":
+        return True
+    
+    if letra == "E":
+        letra = letra.lower()
+        return True
+
+    return False
+    
+# NO MODIFICAR - INICIO
+assert es_vocal_if("a")
+assert not es_vocal_if("b")
+assert es_vocal_if("A")
+assert es_vocal_if("e")
+assert es_vocal_if("E")
+
+# IMPRIMIMOS
+print("EJERCICIO1 1: ", es_vocal_if("a"))
+print("EJERCICIO1 1: ", es_vocal_if("b"))
+print("EJERCICIO1 1: ", es_vocal_if("A"))
+print("EJERCICIO1 1: ", es_vocal_if("e"))
+print("EJERCICIO1 1: ", es_vocal_if("E"))
+print("\n")
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def es_vocal_if_in(letra: str) -> bool:
+    """Re-escribir utilizando un sólo IF y el operador IN.
+    Restricciónes:
+        - Utilizar un único IF.
+        - Utilizar dos returns.
+        - No utilizar ELSE.
+        - No utilizar FOR.
+        - No utilizar listas.
+    Referencia: https://docs.python.org/3/reference/expressions.html#membership-test-operations # noqa: E501
+    """
+    
+    if letra.lower() in "aeiou":
+        return True
+    return False
+    
+
+# NO MODIFICAR - INICIO
+assert es_vocal_if_in("a")
+assert not es_vocal_if_in("b")
+assert es_vocal_if_in("A")
+
+# IMPRIMIMOS
+print("EJERCICIO 2: ", es_vocal_if_in("a"))
+print("EJERCICIO 2: ", es_vocal_if_in("b"))
+print("EJERCICIO 2: ", es_vocal_if_in("A"))
+print("\n")
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def es_vocal_in(letra: str) -> bool:
+    """Re-escribir como expresión booleana utilizando el operador IN
+    Restricciónes:
+        - No utilizar IF.
+        - Utilizar un único return.
+        - No utilizar FOR.
+        - No utilizar listas.
+    """
+    
+    return letra.lower() in "aeiou"
+
+# NO MODIFICAR - INICIO
+assert es_vocal_in("a")
+assert not es_vocal_in("b")
+assert es_vocal_in("A")
+
+# IMPRIMIMOS
+print("EJERCICIO 3: ", es_vocal_if_in("a"))
+print("EJERCICIO 3: ", es_vocal_if_in("b"))
+print("EJERCICIO 3: ", es_vocal_if_in("A"))
+# NO MODIFICAR - FIN
